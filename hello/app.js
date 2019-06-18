397f00cb3ac40bfacd64201faa9cf61f4db84ef8
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2019-06-18 16:59:03 +0300

    JS

---
+++ hello/app.js
@@ -0,0 +1,8 @@
+var msg = 'Hello World';
+console.log(msg);
+
+var i = 0;
+while (i < 10) {
+    console.log( i );
+  i++;
+}
--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-alert("Hello")
+var msg = 'Hello World';
+console.log(msg);
